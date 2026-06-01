a68b1ba8a027255d0618d25569de1103afb57877
Franck1-byte <charesfranck1@gmail.com>
2026-06-01 10:10:25 +0100

    feat: repositoy

--- Backend/src/main/java/com/reseau/painting/painting/repository/MateriauRepository.java
+++ Backend/src/main/java/com/reseau/painting/painting/repository/MateriauRepository.java
@@ -1,8 +1,9 @@
-package com.bcaas.metier.repository;
-import com.bcaas.metier.entite.Materiau;
+package com.reseau.painting.painting.repository;
+import com.reseau.painting.painting.entity.Materiau;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+
 @Repository public interface MateriauRepository extends JpaRepository<Materiau, Long> {
     Optional<Materiau> findByNom(String nom);
     boolean existsByNom(String nom);
