c3b5504cc32c80286a5a6dbc16666bdb547a3283
Franck1-byte <charesfranck1@gmail.com>
2026-06-01 10:32:10 +0100

    mise a jour

--- Backend/src/main/java/com/reseau/painting/painting/repository/TagRepository.java
+++ Backend/src/main/java/com/reseau/painting/painting/repository/TagRepository.java
@@ -1,5 +1,5 @@
-package com.painting.metier.repository;
-import com.bcaas.metier.entite.Tag;
+package com.reseau.painting.painting.repository;
+import com.reseau.painting.painting.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,6 +8,6 @@ import java.util.Optional;
 @Repository public interface TagRepository extends JpaRepository<Tag, Long> { 
     Optional<Tag> findByValeur(String valeur);
     boolean existsByValeur(String valeur);
-    // Chercher tous les tags dont la valeur contient un mot (recherche partielle)
+    // Chercher tous les tags dont la valeur contient un mot (recherche)
     List<Tag> findByValeurContainingIgnoreCase(String mot);
 }
